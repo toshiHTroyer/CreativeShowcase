fc1d3e50fabb2ec1825e270c5881a5307885fab6
toshiHTroyer <124972718+toshiHTroyer@users.noreply.github.com>
2025-04-29 22:47:40 +0000

    uploads more cleaned

--- creativeshowcase/.next/server/middleware-build-manifest.js
+++ creativeshowcase/.next/server/middleware-build-manifest.js
@@ -29,11 +29,6 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/portfolio/[slug].js"
-    ],
-    "/portfolio/settings": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/portfolio/settings.js"
     ]
   },
   "ampFirstPages": []
